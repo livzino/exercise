f6a6ce4be6b299450d7e87a752bd2b91a6911176
Liv <livbendix@live.dk>
2024-04-05 11:23:46 +0200

    jrgikr

---
+++ recipes/modules/settings.js
@@ -0,0 +1,2 @@
+export const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cndwdGlib3RseGx2Y2RlaWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ3OTI4MDYsImV4cCI6MTk4MDM2ODgwNn0.FuHj1T6qJO-wQ_aWaaXNFVfZPG45FsnE3RvHd3PGQmA";
+export const endpoint = "https://uwrwptibotlxlvcdeicv.supabase.co/rest/v1/recipes";
